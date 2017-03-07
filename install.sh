b22e969d6f9863abece4175c6041f2bebb3db43b
Priyank Gandhi <priyank.gandhi@drcsystems.com>
2017-03-07 18:19:20 +0530

    Installation changes

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 VERSION=0.1.0
 EXTENSION=codesnippet
-NAME="Swift3Snippets"
+NAME="Swift3Snippets-master"
 DOWNLOAD_URI=https://github.com/Priyank2016/Swift3Snippets/archive/master.tar.gz
 DIR="${HOME}/Library/Developer/Xcode/UserData/CodeSnippets"
 
