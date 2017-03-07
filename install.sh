b6b9e5410cfe3ffc40613ae098361ffcca49357a
Priyank Gandhi <priyank.gandhi@drcsystems.com>
2017-03-07 17:45:20 +0530

    Change installation file

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ set -euo pipefail
 
 VERSION=0.1.0
 EXTENSION=codesnippet
-NAME="SwiftSnippets-${VERSION}"
-DOWNLOAD_URI=https://github.com/hyperoslo/SwiftSnippets/archive/${VERSION}.tar.gz
+NAME="Swift3Snippets-${VERSION}"
+DOWNLOAD_URI=https://github.com/Priyank2016/Swift3Snippets/archive/${VERSION}.tar.gz
 DIR="${HOME}/Library/Developer/Xcode/UserData/CodeSnippets"
 
 mkdir -p "${DIR}"
 curl -L $DOWNLOAD_URI | tar xvz -C "${DIR}";cp -r "${DIR}/${NAME}/Snippets/" "${DIR}"
+
